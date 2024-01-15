dc694b972ef4b2cb6f08790a1fb2885a2e5db7ac
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-15 13:51:26 -0400

    Testing is fun 💞

--- src/callApi.ts
+++ src/callApi.ts
@@ -1,6 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
+
+// export const getMessageFromServer = async (name: string) => {
+//   const response = await axios.post("/api/message", { name });
+//   return response.data;
+// };
 
 export const getMessageFromServer = async (name: string) => {
-  const response = await axios.post("/api/message", { name });
-  return response.data;
+  const response = await fetch(new URL("/api/message", location.href), {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  });
+  const data = await response.json();
+
+  return data;
 };
