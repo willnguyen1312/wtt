ac0edfd5d3ba298c1f6f6b9a667a2d7ae74992a9
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-14 17:31:35 -0600

    🤘

--- src/tests/FunctionVsComponent.test.tsx
+++ src/tests/FunctionVsComponent.test.tsx
@@ -17,3 +17,28 @@ test("FunctionComponent", () => {
   const result = Component();
   expect(result).not.toBeNull();
 });
+
+const Component2 = () => {
+  return null;
+};
+
+const FunctionReturnsComponent = () => {
+  // This returns null
+  return Component2();
+};
+
+const FunctionComponent2 = () => {
+  // This returns an object so-called VNODE in virtual DOM language
+  return <Component2 />;
+};
+
+// This evaluate to false
+const first = Boolean(FunctionReturnsComponent());
+
+// This evaluate to true
+const second = Boolean(FunctionComponent2());
+
+test("tada", () => {
+  expect(first).toBeFalsy();
+  expect(second).toBeTruthy();
+});
