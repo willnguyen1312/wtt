fc9ee1d33f47b94533be3b8c1059b8d42e6257ce
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-09-17 19:43:16 -0600

    xD

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
+import MillionLint from '@million/lint';
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-
+const _plugins = [nodePolyfills(), react({
+  babel: {
+    plugins: [["module:@preact/signals-react-transform"]]
+  }
+})];
+_plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [
-    nodePolyfills(),
-    react({
-      babel: {
-        plugins: [["module:@preact/signals-react-transform"]],
-      },
-    }),
-  ],
+  plugins: _plugins
 });
